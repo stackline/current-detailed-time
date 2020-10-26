a4f9ee015620f627228a7f449f116b2f8ab4a97d
stackline <stackline@users.noreply.github.com>
2020-10-27 01:10:50 +0900

    fix(format): unify the format so that the difference between the two times can be calculated easily

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	nanosec := now.UnixNano()
 	less_than_one_sec := nanosec - (sec * 1000 * 1000 * 1000)
 
-	fmt.Printf("%d.%d\n", sec, less_than_one_sec)
+	fmt.Printf("%d.%09d\n", sec, less_than_one_sec)
 }
